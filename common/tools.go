135f3da28ab613120ca9996d530e7cad71679a27
Mederic de Verdilhac <mederic.deverdilhac@orange.com>
2024-12-02 17:05:23 +0100

    Update

--- common/tools.go
+++ common/tools.go
@@ -91,3 +91,11 @@ func FindAllSubstringIndexes(s string, sub string) []int {
 	}
 	return indexes
 }
+
+// Absolute value
+func Abs(val int) int {
+	if val < 0 {
+		return val * -1
+	}
+	return val
+}
--- day02/main.go
+++ day02/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"adventofcode2024/common"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-const DAY = "xx"
-const TEST = true
+const DAY = "02"
+const TEST = false
 
 func main() {
 
@@ -18,27 +20,80 @@ func main() {
 	}
 
 	data := common.ReadFileByLine(filePath)
-	parsed := parseData(data)
+	reports := parseData(data)
 
-	result1 := step1(parsed)
+	result1 := step1(reports)
 	fmt.Println("Result1 = ", result1)
 
-	result2 := step2(parsed)
+	result2 := step2(reports)
 	fmt.Println("Result2 = ", result2)
 }
 
-func parseData(data []string) []string {
-	return data
-}
-
-func step1(data []string) int {
-	fmt.Println(data)
-	result := 0
+func parseData(lines []string) [][]int {
+	result := [][]int{}
+	for _, line := range lines {
+		stages := []int{}
+		for _, char := range strings.Split(line, " ") {
+			val, _ := strconv.Atoi(string(char))
+			stages = append(stages, val)
+		}
+		result = append(result, stages)
+	}
 	return result
 }
 
-func step2(data []string) int {
-	fmt.Println(data)
-	result := 0
-	return result
+func isSafe(report []int) int {
+	direction := ""
+	if report[1]-report[0] > 0 {
+		direction = "+"
+	} else {
+		direction = "-"
+	}
+	for idx := 0; idx < len(report)-1; idx++ {
+		diff := report[idx+1] - report[idx]
+		if diff == 0 || common.Abs(diff) > 3 {
+			return idx
+		}
+		if report[idx+1] < report[idx] && direction == "+" {
+			return idx
+		}
+		if report[idx+1] > report[idx] && direction == "-" {
+			return idx
+		}
+	}
+	return -1
+}
+
+func step1(reports [][]int) int {
+	count := 0
+	for _, report := range reports {
+		// fmt.Println(report, isSafe(report))
+		if isSafe(report) == -1 {
+			count++
+		}
+	}
+	return count
+}
+
+func step2(reports [][]int) int {
+	count := 0
+	for _, report := range reports {
+		isSafeOrError := isSafe(report)
+		if isSafeOrError == -1 {
+			count++
+		} else {
+			partial := report[:isSafeOrError]
+			partial = append(partial, report[isSafeOrError+1:]...)
+			if isSafe(partial) == -1 {
+				count++
+			} else {
+				partial := report[:isSafeOrError+1]
+				partial = append(partial, report[isSafeOrError+2:]...)
+				if isSafe(partial) == -1 {
+					count++
+				}
+			}
+		}
+	}
+	return count
 }
